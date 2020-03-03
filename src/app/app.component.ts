400f22b02d9065570e582c17b8921b00b3a8c08e
Novotný David <danovotny@trask.cz>
2020-03-03 12:07:27 +0100

    var boolean out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bootstrapTrial';
-  clicked = false;
   iconClass = 'glyphicon glyphicon-star-empty';
 
   changeIcon() {

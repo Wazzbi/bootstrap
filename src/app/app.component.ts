b5985904d4e6ff56e16c3cbce84671a25276381a
Novotný David <danovotny@trask.cz>
2020-03-03 11:50:55 +0100

    button test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bootstrapTrial';
+  clicked = false;
+  iconClass = 'glyphicon glyphicon-star-empty';
+
+  changeIcon() {
+    const icon =
+      this.iconClass === 'glyphicon glyphicon-star-empty'
+        ? 'glyphicon glyphicon-star'
+        : 'glyphicon glyphicon-star-empty';
+    this.iconClass = icon;
+  }
 }
